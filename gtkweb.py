ff44ae07acc14bce6c7d1e9b0d18c568620b09c6
Nathan Jones <nathan@ncjones.com>
2011-11-13 16:28:37 +1300

    Improving Python API documentation.

--- gtkweb.py
+++ gtkweb.py
@@ -25,12 +25,26 @@ class WebWidget(gtk.Bin):
 	"""A GTK widget that is rendered by a web page.
 
 	The widget provides an interface to a JavaScript object within the page
-	to allow JavaScript method invokation and event subscription.
+	to allow JavaScript method invokation, error handling and event
+	subscription.
 
-	The widget is not thread safe.
+	Although this class can be instantiated directly it is intended to be used
+	as a base type for specific widget implementations. In particular, event
+	handling can only be achieved via sub-typing.
+	
+	Implementors must also provide an HTML page which declares the Javascript
+	widget implementation and registers it using the WebWidget JavaScript API
+	method: GtkWebWidget.register.
+	
 	"""
 	
 	def __init__(self, uri):
+		"""Create a new WebWidget.
+		
+		Args:
+			uri (str): the URI of the widget's HTML page.
+			
+		"""
 		gtk.Bin.__gobject_init__(self)
 		self._web_view = webkit.WebView()
 		self._web_view.load_uri(uri)
@@ -62,13 +76,34 @@ class WebWidget(gtk.Bin):
 			self._handle_result(message_content["result-status"], message_content["result-value"])
 			
 	def handle_event(self, event_type, event_data):
+		"""Process an event that has been propagated from the web view.
+		
+		This method must be implemented by a sub-type in order to enable event
+		handling. Typically the implementor should handle the event by emitting
+		a custom GTK event.
+		
+		Args:
+			event_type (str): the type of event.
+			event_data (object): the data object attached to the event.
+		
+		"""
 		pass
 
 	def _handle_result(self, status, value):
 		self._result_stack.append(_InvokationResult(status == "success", value))
 
 	def invoke(self, function_name, *args):
-		"""Invoke a JavaScript method on the widget."""
+		"""Invoke a JavaScript method on the widget.
+		
+		Args:
+			function_name (str): the name of the JavaScript function to invoke.
+			*args: the arguments to pass to the JavaScript function. Arguments
+				should be compatible with JavaScript primitive types - number,
+				boolean, string, array or associative array.
+		Raises:
+			InvokationFailure: if the method invokation resulted in an error.
+			
+		"""
 		script = "GtkWebWidget.invoke(" + simplejson.dumps(function_name) + ", " + simplejson.dumps(args) + ")"
 		self._web_view.execute_script(script)
 		result = self._result_stack.pop()
@@ -84,6 +119,8 @@ class _InvokationResult(object):
 		self.value = value
 
 class InvokationFailure(Exception):
+	
+	"""Indicates an error occurred while invoking a JavaScript method."""
 
 	def __init__(self, javascriptError):
 		Exception.__init__(self, str(javascriptError))
